4e86473636ca7808146cc05bd1f3ef6ffbd7d49d
pipapo-888 <kazuya2002uk@gmail.com>
2026-03-31 15:40:35 +0900

    Time: 8 ms (95.46%), Space: 9.2 MB (10.67%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.c
+++ 0016-3sum-closest/0016-3sum-closest.c
@@ -1,22 +1,60 @@
-int threeSumClosest(int* nums, int numsSize, int target) {
-    int ans;
-    int how_close = 99999;
+int cmp(const void* x, const void* y)
+{
+	if (*(int*)x > * (int*)y)
+	{
+		return 1;
+	}
+	else if (*(int*)x < *(int*)y)
+	{
+		return -1;
+	}
+	else
+	{
+		return 0;
+	}
+}
 
-    for (int i = 0; i < numsSize - 2; i++)
-    {
-        for (int j = i + 1; j < numsSize - 1; j++)
+int threeSumClosest(int* nums, int numsSize, int target) {
+
+    int ans = nums[0] + nums[1] + nums[numsSize - 1];
+
+    if (numsSize == 3)
+        return (nums[0] + nums[1] + nums[2]);
+    qsort(nums, numsSize, sizeof(int), cmp);
+
+   for (int i = 0; i < numsSize - 2; i++)
+   {
+        int left = i + 1, right = numsSize -1;
+
+        while (left < right)
         {
-            for (int k = j + 1; k < numsSize; k++)
-            {
-                int temp = abs((nums[i] + nums[j] + nums[k]) - target);
-                if (temp < how_close)
-                {
-                    how_close = temp;
-                    ans = nums[i] + nums[j] + nums[k];
-                }
-            }
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum == target)
+                return (sum);
+            if (abs(sum - target) < abs(ans - target))
+                ans = sum;
+            if (sum > target)
+                right--;
+            else
+                 left++;    
         }
-    }
+   }
+
+    // for (int i = 0; i < numsSize - 2; i++)
+    // {
+    //     for (int j = i + 1; j < numsSize - 1; j++)
+    //     {
+    //         for (int k = j + 1; k < numsSize; k++)
+    //         {
+    //             int temp = abs((nums[i] + nums[j] + nums[k]) - target);
+    //             if (temp < how_close)
+    //             {
+    //                 how_close = temp;
+    //                 ans = nums[i] + nums[j] + nums[k];
+    //             }
+    //         }
+    //     }
+    // }
 
     return (ans);
 }
