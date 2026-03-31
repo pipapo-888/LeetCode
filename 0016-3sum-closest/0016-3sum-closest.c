ebb81941663974f9df85f234a3ba7a425c58c90a
pipapo-888 <kazuya2002uk@gmail.com>
2026-03-31 15:42:34 +0900

    Time: 15 ms (57.32%), Space: 9.1 MB (10.67%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.c
+++ 0016-3sum-closest/0016-3sum-closest.c
@@ -11,16 +11,10 @@ int cmp(const void* x, const void* y)
 int threeSumClosest(int* nums, int numsSize, int target) {
     int ans = nums[0] + nums[1] + nums[numsSize - 1];
 
-    if (numsSize == 3)
-        return (nums[0] + nums[1] + nums[2]);
     qsort(nums, numsSize, sizeof(int), cmp);
-
-   for (int i = 0; i < numsSize - 2; i++)
-   {
+   for (int i = 0; i < numsSize - 2; i++) {
         int left = i + 1, right = numsSize -1;
-
-        while (left < right)
-        {
+        while (left < right) {
             int sum = nums[i] + nums[left] + nums[right];
             if (sum == target)
                 return (sum);
