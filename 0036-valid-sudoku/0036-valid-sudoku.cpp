89d01da7ee532aa3d697b39d1bb4d2b06d9c61c3
pipapo-888 <kazuya2002uk@gmail.com>
2026-05-01 22:16:35 +0900

    Time: 0 ms (100%), Space: 21.8 MB (73.3%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,56 +1,22 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        for (int i = 0; i < 9; i++)
-        {
-            vector<int> box(10, 0);
-            for (int j = 0; j < 9; j++)
-            {
-                if (board[i][j] == '.')
-                    continue ;
-                else
-                    box[board[i][j] - '0']++;
-                if (box[board[i][j] - '0'] > 1)
-                    return false;
-            }
-        }
-        for (int i = 0; i < 9; i++)
-        {
-            vector<int> box(10, 0);
-            for (int j = 0; j < 9; j++)
-            {
-                if (board[j][i] == '.')
-                    continue ;
-                else
-                    box[board[j][i] - '0']++;
-                if (box[board[j][i] - '0'] > 1)
-                    return false;
-            }
-        }
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                vector<int> box(10, 0);
-                for (int row = 0; row < 3; row++)
-                {
-                    for (int col = 0; col < 3; col++)
-                    {
-                        int x = i * 3 + row;
-                        int y = j * 3 + col;
 
-                    if (board[x][y] == '.')
-                        continue ;
-                    else
-                        box[board[x][y] - '0']++;
-                    if (box[board[x][y] - '0'] > 1)
+        bool rows [9][9] = {false};
+        bool cols [9][9] = {false};
+        bool boxes [9][9] = {false};
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxNum = (i / 3) * 3 + (j / 3);
+                    if (rows[i][num] || cols[j][num] || boxes[boxNum][num])
                         return false;
-                    }
+                    rows[i][num] = cols[j][num] = boxes[boxNum][num] = true;
                 }
             }
         }
-
-
         return true;
     }
 };
