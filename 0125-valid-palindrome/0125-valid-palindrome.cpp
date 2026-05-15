7375afa8a64961b87cab0daf9b9f10669038c357
pipapo-888 <kazuya2002uk@gmail.com>
2026-05-15 21:45:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {  
+    bool isPalindrome(string s) {
         int right = s.size() - 1;
         int left = 0;
+
+        
+
         while (left < right)
         {
-            while (!isalnum(s[left]) && left < right)
+            while(!isalpha(s[left]) && left <= right)
                 left++;
-            while(!isalnum(s[right]) && left < right)
+            while(!isalpha(s[right]) && left <= right)
                 right--;
             if (toupper(s[left]) != toupper(s[right]))
                 return false;
-            left++, right--;
+            left++;
+            right--;
         }
         return true;
     }
